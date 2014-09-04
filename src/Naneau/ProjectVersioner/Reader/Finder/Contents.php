6e27590c3f1fc239dc44596a5fbf99b544062cc1
Maurice Fonk <git@naneau.net>
2014-09-04 19:41:43 -0400

    adding a file contents based reader

---
+++ src/Naneau/ProjectVersioner/Reader/Finder/Contents.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Contents.php
+ *
+ * @package         ProjectVersioner
+ * @subpackage      Reader
+ */
+
+namespace Naneau\ProjectVersioner\Reader\Finder;
+
+use Naneau\ProjectVersioner\Reader\Finder\Finder;
+
+/**
+ * Contents
+ *
+ * Combines contents from all found files
+ *
+ * @category        Naneau
+ * @package         ProjectVersioner
+ * @subpackage      Reader
+ */
+class Contents extends Finder
+{
+    /**
+     * {@inheritdoc}
+     **/
+    public function read($directory)
+    {
+        $contents = '';
+        foreach ($this->getFinder() as $file) {
+
+            $fileContents = file_get_contents(
+                $file->getPath()
+                . DIRECTORY_SEPARATOR . $file->getFilename()
+            );
+
+            $contents = md5($contents . $fileContents);
+        }
+
+        return substr($contents, 0, 6);
+    }
+}
--- tests/Reader/FinderTest.php
+++ tests/Reader/FinderTest.php
@@ -2,12 +2,13 @@
 
 use Naneau\ProjectVersioner\Versioner;
 use Naneau\ProjectVersioner\Reader\Finder\MTime as MTimeReader;
+use Naneau\ProjectVersioner\Reader\Finder\Contents as ContentsReader;
 
 use Symfony\Component\Finder\Finder;
 
 class FinderTest extends \PHPUnit_Framework_TestCase
 {
-    public function testRead()
+    public function testMtime()
     {
         $directory = __DIR__ . '/../projects/finder';
 
@@ -23,5 +24,20 @@ class FinderTest extends \PHPUnit_Framework_TestCase
         $versioner = new Versioner($directory, $readers);
 
         $this->assertEquals($time, $versioner->get());
+    }
+
+    public function testContents()
+    {
+        $directory = __DIR__ . '/../projects/finder';
+
+        $finder = new Finder;
+        $finder->name('*.php');
+
+        $readers = array(new ContentsReader($finder));
+
+        $versioner = new Versioner($directory, $readers);
+
+        $hash = 'ee53e4';
+        $this->assertEquals($hash, $versioner->get());
     }
 }
